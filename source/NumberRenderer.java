aa025c1ec195a483664a2063318cf422d5db0780
tips4java <142698712+tips4java@users.noreply.github.com>
2023-08-20 14:52:47 -0400

    Add files via upload

---
+++ source/NumberRenderer.java
@@ -0,0 +1,39 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import javax.swing.SwingConstants;
+
+public class NumberRenderer extends FormatRenderer
+{
+	/*
+	 *  Use the specified number formatter and right align the text
+	 */
+	public NumberRenderer(NumberFormat formatter)
+	{
+		super(formatter);
+		setHorizontalAlignment( SwingConstants.RIGHT );
+	}
+
+	/*
+	 *  Use the default currency formatter for the default locale
+	 */
+	public static NumberRenderer getCurrencyRenderer()
+	{
+		return new NumberRenderer( NumberFormat.getCurrencyInstance() );
+	}
+
+	/*
+	 *  Use the default integer formatter for the default locale
+	 */
+	public static NumberRenderer getIntegerRenderer()
+	{
+		return new NumberRenderer( NumberFormat.getIntegerInstance() );
+	}
+
+	/*
+	 *  Use the default percent formatter for the default locale
+	 */
+	public static NumberRenderer getPercentRenderer()
+	{
+		return new NumberRenderer( NumberFormat.getPercentInstance() );
+	}
+}
